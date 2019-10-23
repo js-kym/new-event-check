e2bff53d789991fe1590935fdeaef626f65e27b3
kym <>
2019-10-23 16:34:48 +0900

    jsonpをmain.jsに移動

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import VueJsonp from 'vue-jsonp';
+
+Vue.use(VueJsonp);
 
 Vue.config.productionTip = false;
 
